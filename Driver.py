b984ae7be435dd01e7ecb57b9fc2060ecb31390e
Matt <matt.mcwaters@mail.utoronto.ca>
2016-04-05 21:13:27 -0400

    Added variable init and some constraint init

--- Driver.py
+++ Driver.py
@@ -3,9 +3,24 @@ import cspbase
 import propagators
 
 def main():
-    Input = []
-    CSP = converter.toCSP(Input)
-    assignments = propagators.prop_GAC(CSP)
+    board = [[0, 0,0,0,0,0],
+             [-1,0,0,0,-1,0],
+             [0,1,0,0,-1,0],
+             [0,0,0,0,0,-1],
+             [-1,0,0,0,1,0],
+             [-1,0,0,-1,0,0]]
+
+    print(board)
+
+    CSP = converter.toCSP(board)
+    # prop = propagators.prop_GAC
+    # backtracker = cspbase.BT(CSP)
+    # backtracker.bt.search(prop)
+    # backtracker.trace_on()
+    #
+    # for var in CSP.get_all_vars():
+    #     print(var.name)
+    #     print("Value = {}".format(var.assignedValue))
 
 if __name__ == "__main__":
     main()
--- converter.py
+++ converter.py
@@ -1,1 +1,45 @@
+from cspbase import *
 
+
+def toCSP(board):
+    finalCsp = CSP("3-in-a-row")
+    vars = []
+    consSameList = []
+    varsWithStructure = []
+
+    domain = [-1, 0, 1]
+    count = 0
+    for row in board:
+        temp = []
+        for i in range(len(row)):
+            count += 1
+            var = Variable("V{}".format(count))
+            if row[i] != 0:
+                var.add_domain_values([row[i]])
+            else:
+                var.add_domain_values(domain)
+            temp.append(var)
+            vars.append(var)
+            finalCsp.add_var(var)
+        varsWithStructure.append(temp)
+
+    n = len(board)
+    i=0
+    for row in varsWithStructure:
+        i=i+1
+        c = Constraint("EqualColors-Row{}".format(i), row)
+        consSameList.append(c)
+
+    t=()
+    for element in varsWithStructure:
+        t += (element,)
+    transposed = [list(x) for x in zip(*t)]
+
+    i=0
+    for column in transposed:
+        i=i+1
+        c = Constraint("EqualColors-Column{}".format(i), column)
+        consSameList.append(c)
+
+
+    print(1)
